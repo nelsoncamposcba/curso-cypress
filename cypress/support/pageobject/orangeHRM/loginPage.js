dbff088d6cc925b9614ff2e3dc3737f799a57fc3
Nelson Campos <nelsoncamposcapa@gmail.com>
2022-03-20 12:01:41 -0300

    Fin clase 3

---
+++ cypress/support/pageobject/orangeHRM/loginPage.js
@@ -0,0 +1,23 @@
+class LoginPage {
+
+    elementos = {
+            txt_userName : () => cy.get('#txtUsername'),
+            txt_password: () => cy.get('#txtPassword'),
+            btn_login: () => cy.get('#btnLogin'),
+            lbl_user: () => cy.get('#welcome')
+        }
+        
+        type_username(name){
+            this.elementos.txt_userName().type(name)
+        }
+        type_password(pass){
+            this.elementos.txt_password().type(pass)
+        }
+        
+        click_login(){
+            this.elementos.btn_login().click()
+        }
+
+}
+
+module.exports = new LoginPage();
